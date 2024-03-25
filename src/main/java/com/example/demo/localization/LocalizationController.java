f109dfec7f4824bf17cbdcc0a28a3f388cb53d0c
michal.paukszto <michal.paukszto@gmail.com>
2024-03-25 16:41:10 +0100

    review

--- src/main/java/com/example/demo/localization/LocalizationController.java
+++ src/main/java/com/example/demo/localization/LocalizationController.java
@@ -6,89 +6,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/localizations")
 public class LocalizationController implements LocalizationsApi {
+
     private final LocalizationService localizationService;
+
+    // todo handle exceptions -> create a class annotated with @RestControllerAdvice
 
     @Override
     public ResponseEntity<List<LocalizationDTO>> localizationsGet() throws Exception {
+        // todo use 'var'
         List<LocalizationDTO> localizationsDTO = localizationService.localizationsGet();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(localizationsDTO);
     }
 
     @Override
-    public ResponseEntity<LocalizationDTO> localizationsIdGet(@PathVariable Long id) throws Exception {
+    public ResponseEntity<LocalizationDTO> localizationsIdGet(@PathVariable Long id) throws Exception { // todo remove @PathVariable - it is unnecessary
+        // todo add pagination https://www.baeldung.com/rest-api-pagination-in-spring
         LocalizationDTO localizationDTO = localizationService.localizationsIdGet(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(localizationDTO);
     }
 
     @Override
-    public ResponseEntity<LocalizationDTO> localizationsPost(LocalizationDTO localizationDTO) throws Exception {
+    public ResponseEntity<LocalizationDTO> localizationsPost(LocalizationDTO localizationDTO) throws Exception { // todo maxLength=50 in openid -> exception -> @RestControllerAdvice -> 400
         LocalizationDTO createdLocalization = localizationService.localizationsPost(localizationDTO.getName());
         return ResponseEntity.status(HttpStatus.CREATED).body(createdLocalization);
     }
-
-
-
-
-//    @GetMapping("/localizations")
-//    public LocalizationDTO get() {
-//        LocalizationDTO localizationDTO = new LocalizationDTO();
-//        localizationDTO.setName("Slupsk");
-//        return localizationDTO;
-//    }
-
-//    //pobiera listę lokalizacji
-//    @GetMapping
-//    public ResponseEntity<List<LocalizationDTO>> getAllLocalizations() {
-//        LocalizationDTO localizationDTO = new LocalizationDTO();
-//        localizationDTO.setName("Słupsk");
-//        List<LocalizationDTO> localizations = new ArrayList<>();
-//        localizations.add(localizationDTO);
-//        return ResponseEntity.ok(localizations);
-//    }
-//
-//    // Tworzy nową lokalizację
-//    @PostMapping
-//    public ResponseEntity<LocalizationDTO> createLocalization(@RequestBody LocalizationDTO localizationDTO) {
-//        LocalizationDTO createdLocalization = new LocalizationDTO();
-//        createdLocalization.setName(localizationDTO.getName());
-//        return new ResponseEntity<>(createdLocalization, HttpStatus.CREATED);
-//    }
-//
-//    // Pobiera lokalizację po ID
-//    @GetMapping("/{id}")
-//    public ResponseEntity<LocalizationDTO> getLocalizationById(@PathVariable Long id) {
-//        LocalizationDTO localizationDTO = new LocalizationDTO();
-//        localizationDTO.setName("Słupsk");
-//        return ResponseEntity.ok(localizationDTO);
-//    }
-
-
-//    @Override
-//    public ResponseEntity<List<Localization>> localizationsGet() throws Exception {
-//        return ResponseEntity.status(200).body((List<Localization>) new LocalizationDTO("Słupsk"));
-//    }
-
-//    @Override
-//    public ResponseEntity<List<Localization>> localizationsGet() throws Exception {
-//        LocalizationDTO localizationDTO = new LocalizationDTO("Słupsk");
-//        List<LocalizationDTO> localizationsDTOList = new ArrayList<>();
-//        localizationsDTOList.add(localizationDTO);
-//        Localization localization = new Localization();
-//        localization.setName(localizationDTO.getName());
-//        List<Localization> localizationsList = new ArrayList<>();
-//        localizationsList.add(localization);
-//        return ResponseEntity.ok(localizationsList);
-//    }
-
 }
--- src/main/java/com/example/demo/localization/LocalizationService.java
+++ src/main/java/com/example/demo/localization/LocalizationService.java
@@ -11,9 +11,9 @@ import java.util.List;
 public class LocalizationService {
 
     private final LocalizationRepository localizationRepository;
-    private final LocalizationMapper localizationMapper;
+    private final LocalizationMapper localizationMapper; // todo move to the controller, services should return entities, not DTOs
 
-    public LocalizationDTO localizationsPost(String name) {
+    public LocalizationDTO localizationsPost(String name) { // todo rename create
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Nieprawidłowa nazwa lokalizacji: " + name);
         }
@@ -21,12 +21,12 @@ public class LocalizationService {
         return localizationMapper.asDTO(localizationRepository.save(localization));
     }
 
-    public List<LocalizationDTO> localizationsGet() {
+    public List<LocalizationDTO> localizationsGet() { // todo rename getAll
         List<Localization> localizations = localizationRepository.findAll();
         return localizationMapper.asDTO(localizations);
     }
 
-    public LocalizationDTO localizationsIdGet(Long id) {
+    public LocalizationDTO localizationsIdGet(Long id) { // todo rename get
         Localization localization = localizationRepository
                 .findById(id)
                 .orElseThrow(() -> new RuntimeException("Nie ma lokalizacji o id: " + id));
